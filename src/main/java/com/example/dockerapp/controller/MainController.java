da4e30d9d682910219d5ec8fbe7875fb29ba1efb
sivasubrahmanyam <sivasubrahmanyam.pagolu@techwave.net>
2022-04-23 11:22:23 +0530

    first commit

---
+++ src/main/java/com/example/dockerapp/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.dockerapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/")
+public class MainController {
+    @GetMapping("v1/message")
+    public String getMessage(){
+        return "Hello from docker";
+    }
+}
