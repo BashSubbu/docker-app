3c7768ae9d0815070e15354fd967c92a165235dd
sivasubrahmanyam <sivasubrahmanyam.pagolu@techwave.net>
2022-06-15 16:27:00 +0530

    added swagger configuration

--- src/main/java/com/example/dockerapp/controller/MainController.java
+++ src/main/java/com/example/dockerapp/controller/MainController.java
@@ -3,6 +3,9 @@ package com.example.dockerapp.controller;
 import com.example.dockerapp.exception.GeneralException;
 import com.example.dockerapp.imports.ImportsManager;
 import com.google.gson.Gson;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,16 +13,16 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/")
+@RequestMapping("/api")
 public class MainController {
     @Autowired
     private ImportsManager importsManager;
 
-    @GetMapping("v1/message")
+    @GetMapping("/v1/message")
     public String getMessage(){
         return "Hello from Docker";
     }
-    @GetMapping("v1/messages")
+    @GetMapping("/v1/messages")
     public String getMessages(){
         return "Messages from dockers ....";
     }
@@ -30,7 +33,10 @@ public class MainController {
      * @return
      * @throws Exception
      */
-    @PostMapping("v1/import")
+    @ApiOperation(value = "Import Products", tags = "File Import", response = String.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Imported product Successfully")
+    })
+    @PostMapping("/v1/import")
     public String importProducts(@RequestParam(name = "multipartFile") MultipartFile multipartFile) {
         List<String> productIdList = null;
         Gson gson = new Gson();
